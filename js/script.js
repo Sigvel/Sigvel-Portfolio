4495c7f403ea5877a4590e032e7c7631135f4f0a
Tony <89355439+Sigvel@users.noreply.github.com>
2022-06-11 17:25:01 +0200

    Fixed the navigation and some other aspects in about and slider items

--- js/script.js
+++ js/script.js
@@ -10,3 +10,44 @@ arrowNext.addEventListener("click", () => {
 arrowPrevious.addEventListener("click", () => {
   itemsContainer.prepend(sliderItem[sliderItem.length - 1]);
 });
+
+// Nav on scrolling show
+const nav = document.querySelector("nav");
+
+function handleScroll() {
+  const scrolledY = window.scrollY;
+
+  if (scrolledY > 500) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+}
+
+window.addEventListener("scroll", handleScroll);
+
+// Current on section
+
+const sections = document.querySelectorAll("section");
+const navListItem = document.querySelectorAll("nav li");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (window.scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+      console.log(current);
+    }
+  });
+
+  navListItem.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
+
+// Contact Page
